7a4e8690f62bf9350b67efb9ce9d066610ea4b4b
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-04-24 15:54:07 +0500

    re commit

--- src/components/shared/SectionTitle/SectionTitle.jsx
+++
@@ -1,9 +0,0 @@
-export const SectionTitle = ({ subTitle, title, body }) => {
-  return (
-    <div className='trending-top'>
-      <span className='saint-text'>{subTitle}</span>
-      <h2>{title}</h2>
-      <p>{body}</p>
-    </div>
-  );
-};

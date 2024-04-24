39ceb9294f0d70caa48de9c85653cdf610c2f537
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-04-24 13:36:44 +0500

    first commit

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['i.ibb.co','lh3.googleusercontent.com','res.cloudinary.com'],
-  },
-}
-
-module.exports = nextConfig
